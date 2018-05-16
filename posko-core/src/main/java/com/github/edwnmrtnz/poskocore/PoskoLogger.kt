53d6880c7a22baab116d8039e98055689f6d35fa
Edwin Martinez <dev.edwinmartinez@gmail.com>
2018-05-17 00:02:55 +0800

    Add simple logger

---
+++ posko-core/src/main/java/com/github/edwnmrtnz/poskocore/PoskoLogger.kt
@@ -0,0 +1,24 @@
+package com.github.edwnmrtnz.poskocore
+
+import android.util.Log
+
+class PoskoLogger {
+
+    companion object {
+        val TAG = "PoskoLogger"
+
+        @JvmStatic
+        fun log(message : String){
+
+            Log.d(TAG, message)
+        }
+
+        @JvmStatic
+        fun log(TAG : String, message: String){
+
+            log(message)
+
+            Log.d(TAG, message)
+        }
+    }
+}
