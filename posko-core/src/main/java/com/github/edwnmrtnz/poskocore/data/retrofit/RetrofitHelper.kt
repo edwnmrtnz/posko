2f7e7ff05046f454af22dd45f0505352974d8bb5
Edwin Martinez <dev.edwinmartinez@gmail.com>
2018-05-17 23:25:57 +0800

    Finish injectable RetrofitHelper

--- posko-core/src/main/java/com/github/edwnmrtnz/poskocore/data/retrofit/RetrofitHelper.kt
+++ posko-core/src/main/java/com/github/edwnmrtnz/poskocore/data/retrofit/RetrofitHelper.kt
@@ -1,5 +1,13 @@
 package com.github.edwnmrtnz.poskocore.data.retrofit
 
+import com.github.edwnmrtnz.poskocore.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+
 class RetrofitHelper {
     companion object {
         @JvmStatic
@@ -22,4 +30,29 @@ class RetrofitHelper {
             if(INSTANCE != null) INSTANCE = null
         }
     }
+
+    fun <S> createService(BASE_URL : String, basicAuth : String?, serviceClass : Class<S>, converterFactory : Converter.Factory) : S {
+        val builder : Retrofit.Builder = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(converterFactory)
+        if(basicAuth != null){
+            var httpClient : OkHttpClient.Builder = OkHttpClient.Builder()
+            httpClient.addInterceptor(Interceptor {
+                var original : Request = it.request()
+
+                var requestBuilder : Request.Builder = original.newBuilder().header("Authorization", basicAuth)
+                requestBuilder.header("Accept", "application/json")
+                requestBuilder.method(original.method(), original.body())
+
+                val request : Request = requestBuilder.build()
+                it.proceed(request)
+            })
+
+            val client : OkHttpClient = httpClient.build()
+            builder.client(client)
+        }
+
+        return builder.build().create(serviceClass);
+    }
+
 }
