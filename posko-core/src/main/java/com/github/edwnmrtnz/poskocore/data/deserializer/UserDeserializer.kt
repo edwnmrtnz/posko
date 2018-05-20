764aa113d302014c28100894bfb05c85f8f07810
Edwin Martinez <dev.edwinmartinez@gmail.com>
2018-05-20 23:35:13 +0800

    Add custom deserializer for user

---
+++ posko-core/src/main/java/com/github/edwnmrtnz/poskocore/data/deserializer/UserDeserializer.kt
@@ -0,0 +1,24 @@
+package com.github.edwnmrtnz.poskocore.data.deserializer
+
+import com.github.edwnmrtnz.poskocore.data.model.User
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import java.lang.reflect.Type
+
+class UserDeserializer : JsonDeserializer<User> {
+
+    override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): User {
+        val jsonObject : JsonObject = json!!.asJsonObject
+
+        val userJsonObject = jsonObject.getAsJsonObject("user")
+
+        return User(userJsonObject.get("email").asString,
+                userJsonObject.get("first_name").asString,
+                userJsonObject.get("last_name").asString,
+                userJsonObject.get("token").asString,
+                userJsonObject.get("auth_token").asString,
+                userJsonObject.get("created_at").asString)
+    }
+}
