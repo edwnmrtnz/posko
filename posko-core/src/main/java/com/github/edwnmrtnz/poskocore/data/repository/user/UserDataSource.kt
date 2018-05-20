e455e73394d7c0537bc6ceaba0ee6083e7212223
Edwin Martinez <dev.edwinmartinez@gmail.com>
2018-05-20 23:34:56 +0800

    Finish authenticateUser api

--- posko-core/src/main/java/com/github/edwnmrtnz/poskocore/data/repository/user/UserDataSource.kt
+++ posko-core/src/main/java/com/github/edwnmrtnz/poskocore/data/repository/user/UserDataSource.kt
@@ -1,4 +1,15 @@
 package com.github.edwnmrtnz.poskocore.data.repository.user
 
+import com.github.edwnmrtnz.poskocore.data.model.User
+
 interface UserDataSource {
+
+    interface AuthenticateUserCallback{
+        fun onAuthenticated(user: User)
+        fun onFailed(message : String, statusCode : Int)
+    }
+
+    fun authenticateUser(account_name : String, email : String, password : String, callback: AuthenticateUserCallback)
+
+
 }
--- posko-core/src/main/java/com/github/edwnmrtnz/poskocore/data/repository/user/UserRepository.kt
+++ posko-core/src/main/java/com/github/edwnmrtnz/poskocore/data/repository/user/UserRepository.kt
@@ -1,4 +1,12 @@
 package com.github.edwnmrtnz.poskocore.data.repository.user
 
-class UserRepository {
+class UserRepository(val userLocalDataSource: UserLocalDataSource, val userRemoteDataSource: UserRemoteDataSource) : UserDataSource {
+    companion object {
+        val TAG = "UserRepository"
+    }
+
+    override fun authenticateUser(account_name: String, email: String, password: String, callback: UserDataSource.AuthenticateUserCallback) {
+        userRemoteDataSource.authenticateUser(account_name, email, password, callback)
+    }
+
 }
