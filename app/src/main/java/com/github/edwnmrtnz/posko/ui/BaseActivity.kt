d567ab6f5f36859d60952f9626b98f4d245e7e0c
Edwin Martinez <dev.edwinmartinez@gmail.com>
2018-05-26 18:38:35 +0800

    Change import

--- app/src/main/java/com/github/edwnmrtnz/posko/ui/BaseActivity.kt
+++ app/src/main/java/com/github/edwnmrtnz/posko/ui/BaseActivity.kt
@@ -3,7 +3,7 @@ package com.github.edwnmrtnz.posko.ui
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import com.github.edwnmrtnz.posko.tools.PoskoLogger
-import com.github.edwnmrtnz.poskocore.PoskoLogger
+
 
 abstract class BaseActivity : AppCompatActivity() {
     val TAG = "Logger"
