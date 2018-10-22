badead3359af1be2d3542f4ae872a1bbd5ccbe36
Edwin Martinez <dev.edwinmartinez@gmail.com>
2018-10-22 21:30:23 +0800

    Add base dao to handle common db operations

--- core/src/main/java/com/github/posko/core/data/database/dao/base/BaseDao.kt
+++ core/src/main/java/com/github/posko/core/data/database/dao/base/BaseDao.kt
@@ -1,4 +1,20 @@
 package com.github.posko.core.data.database.dao.base
 
-interface BaseDao {
+import android.arch.persistence.room.Delete
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Update
+
+interface BaseDao<T> {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(element : T)
+
+    @Insert
+    fun insert(vararg elements : T)
+
+    @Delete
+    fun delete(element : T)
+
+    @Update
+    fun update(element : T)
 }
