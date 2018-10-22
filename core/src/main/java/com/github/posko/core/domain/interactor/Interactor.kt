97bbc455f825f69126904d3709e71c96a094635c
Edwin Martinez <dev.edwinmartinez@gmail.com>
2018-10-22 22:41:43 +0800

    Create base interactor to handle dispatching of coroutines context by default

--- core/src/main/java/com/github/posko/core/domain/interactor/Interactor.kt
+++ core/src/main/java/com/github/posko/core/domain/interactor/Interactor.kt
@@ -1,3 +1,21 @@
 package com.github.posko.core.domain.interactor
 
-class Interactor
+import com.github.posko.core.domain.dispatcher.AppCoroutineDispatcher
+import com.github.posko.core.domain.result.Either
+import com.github.posko.core.domain.result.Error
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
+
+abstract class Interactor<out Response, in Request> (private val dispatcher : AppCoroutineDispatcher) where Response : Any {
+
+    internal abstract suspend fun run (params : Request) : Either<Error, Response>
+
+    fun execute(params : Request, result : (Either<Error, Response>) -> Unit) {
+        val job = async(dispatcher.io()) {
+            run(params)
+        }
+        launch(dispatcher.ui()) {
+            result.invoke(job.await())
+        }
+    }
+}
