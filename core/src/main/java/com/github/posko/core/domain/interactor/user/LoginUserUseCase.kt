758117d06d56ec2b275c68f7669e4e666ad1c923
Edwin Martinez <dev.edwinmartinez@gmail.com>
2018-10-25 07:51:00 +0800

    Add login interactor

--- core/src/main/java/com/github/posko/core/domain/interactor/user/LoginUserUseCase.kt
+++ core/src/main/java/com/github/posko/core/domain/interactor/user/LoginUserUseCase.kt
@@ -1,4 +1,18 @@
 package com.github.posko.core.domain.interactor.user
 
-class LoginUserUseCase {
+import com.github.posko.core.domain.dispatcher.AppCoroutineDispatcher
+import com.github.posko.core.domain.gateways.UserGateway
+import com.github.posko.core.domain.interactor.Interactor
+import com.github.posko.core.domain.model.User
+import com.github.posko.core.domain.result.Either
+import com.github.posko.core.domain.result.Error
+
+class LoginUserUseCase (dispatcher: AppCoroutineDispatcher,
+                        private var gateway: UserGateway) : Interactor<User, LoginUserUseCase.Param>(dispatcher) {
+
+    override suspend fun run(params: Param): Either<Error, User> {
+        return gateway.login(params.accountName, params.email, params.password)
+    }
+
+    data class Param(var accountName : String, var email : String, var password : String)
 }
