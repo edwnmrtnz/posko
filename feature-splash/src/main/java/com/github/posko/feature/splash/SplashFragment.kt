d0f9dc7c4c2f8bac1de6f7be4a8bad69408e46c5
Edwin Martinez <dev.edwinmartinez@gmail.com>
2019-06-18 06:12:09 +0800

    Make all test pass

--- feature-splash/src/main/java/com/github/posko/feature/splash/SplashFragment.kt
+++ feature-splash/src/main/java/com/github/posko/feature/splash/SplashFragment.kt
@@ -1,11 +1,10 @@
 package com.github.posko.feature.splash
 
-
-import android.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 
 // TODO: Rename parameter arguments, choose names that match
