be0be7c1b8df2d734cc4735f6649f97ad52e723d
Edwin Martinez <dev.edwinmartinez@gmail.com>
2019-11-22 23:04:41 +0800

    Finish initial concept design for add to cart

--- feature-addtocart/src/main/java/com/github/posko/feature/addtocart/AddToCartFragment.kt
+++ feature-addtocart/src/main/java/com/github/posko/feature/addtocart/AddToCartFragment.kt
@@ -1,7 +1,6 @@
 package com.github.posko.feature.addtocart
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,6 +19,31 @@ class AddToCartFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        checkRequirement()
+    }
+
+    private fun checkRequirement() {
+        val id = getProductId()
+        check(id >= 1) { "Product id is null" }
+    }
+
+    private fun getProductId(): Int {
+        return arguments!!.getInt(KEY_PRODUCT_ID)
+    }
+
+    companion object {
+
+        private const val KEY_PRODUCT_ID = "key_product_id"
+
+        @JvmStatic
+        fun newInstance(productId: Int): AddToCartFragment {
+            val fragment = AddToCartFragment()
+            val bundle = Bundle()
+            bundle.putInt(KEY_PRODUCT_ID, productId)
+            fragment.arguments = bundle
+            return fragment
+        }
     }
 
 }
